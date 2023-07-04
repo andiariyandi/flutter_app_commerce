0e808e69af161e95ad9b0bc109591bf2ed1a443e
ar. Andi <andiariyandi24@gmail.com>
2023-07-04 08:04:39 +0700

    sign in bloc

---
+++ lib/pages/sign_in/bloc/sign_in_blocs.dart
@@ -0,0 +1,10 @@
+import 'package:ulearning_app/pages/sign_in/bloc/sign_in_events.dart';
+import 'package:ulearning_app/pages/sign_in/bloc/sign_in_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignInBloc extends Bloc<SignInEvent, SignInState> {
+  SignInBloc() : super(const SignInState()) {
+    on<EmailEvent>((event, state) {});
+    on<PasswordEvent>((event, state) {});
+  }
+}
